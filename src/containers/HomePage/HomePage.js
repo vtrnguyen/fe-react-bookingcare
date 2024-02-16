9825dc40b2976638711c4f4c055d6c73debac0d4
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2024-02-16 22:56:34 +0700

    fe-react-bookingcare

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
+import Specialty from './Section/Specialty';
 
 class HomePage extends Component {
 
@@ -9,6 +10,8 @@ class HomePage extends Component {
         return (
             <>
                 <HomeHeader />
+                <Specialty />
+                <div style={{ height: "300px" }}></div>
             </>
         );
     }
