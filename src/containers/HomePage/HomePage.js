23428b54695e4915f3022e6b55a46d5834822090
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2024-02-22 22:57:25 +0700

    fe-react-bookingcare

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
+import MedicalFacility from './Section/MedicalFacility';
+import OutstandingDocter from './Section/OutstandingDocter';
+import HandBook from './Section/HandBook';
+import './HomePage.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { set } from 'lodash';
 
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+        };
 
         return (
             <>
                 <HomeHeader />
-                <Specialty />
-                <div style={{ height: "300px" }}></div>
+                <Specialty 
+                    settings={settings}
+                />
+                <MedicalFacility 
+                    settings={settings}
+                />
+                <OutstandingDocter 
+                    settings={settings}
+                 />
+                <HandBook 
+                    settings={settings}
+                />
             </>
         );
     }
---
+++ src/containers/HomePage/Section/OutstandingDocter.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Slider from 'react-slick';
+
+class OutstandingDoctor extends Component {
+
+    render() {
+        return (
+            <div className="section-share section-outstanding-doctor">
+                <div className="section-container">
+                    <div className="section-header">
+                        <span className="title-section">Bác sĩ nổi bật trong tuần qua</span>
+                        <button className="btn-section">Tìm kiếm</button>
+                    </div>
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, Tiến sĩ Origin Dev 1</div>
+                                        <div>Cơ xương khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, Tiến sĩ Origin Dev 2</div>
+                                        <div>Cơ xương khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, Tiến sĩ Origin Dev 3</div>
+                                        <div>Cơ xương khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, Tiến sĩ Origin Dev 4</div>
+                                        <div>Cơ xương khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, Tiến sĩ Origin Dev 5</div>
+                                        <div>Cơ xương khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="section-customize">
+                                <div className="customize-border">
+                                    <div className="outer-bg">
+                                        <div className="bg-image section-outstanding-doctor" />
+                                    </div>
+                                    <div className="position text-center">
+                                        <div>Giáo sư, Tiến sĩ Origin Dev 6</div>
+                                        <div>Cơ xương khớp</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Slider>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OutstandingDoctor);
