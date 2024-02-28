4f224b51fe2c6455a4afa410f8c8a9779ce5d688
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2024-02-28 21:15:52 +0700

    fe-react-bookingcare

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from "../../utils";
+import { FormattedMessage } from 'react-intl';
  
 class Header extends Component {
 
@@ -14,8 +15,7 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
-
+        const { processLogout, language, userInfo } = this.props;
         return (
             <div className="header-container">
                 {/* thanh navigator */}
@@ -24,6 +24,7 @@ class Header extends Component {
                 </div>
 
                 <div className="languages">
+                    <span className="welcome"><FormattedMessage id="homeheader.welcome" /> { userInfo && userInfo.firstName && userInfo.lastName ? userInfo.firstName + ' ' + userInfo.lastName : '' }</span>
                     <span 
                         className={ language === LANGUAGES.VI ? "language-vi active" : "language-vi" }
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
@@ -50,6 +51,7 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
+        userInfo: state.user.userInfo,
         language: state.app.language,
     };
 };
